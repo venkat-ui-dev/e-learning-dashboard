cb45e2270f9511b43dd0bd242371068259825e0b
venkat <techvenkats@gmail.com>
2024-12-01 20:32:37 +0530

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,8 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
     if (pathname === "/" || pathname === "/dashboard") {
+        console.log(request.url);
+
         return NextResponse.redirect(new URL("/dashboard/student", request.url));
     }
 }
