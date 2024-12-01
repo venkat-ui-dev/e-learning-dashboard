57d385698d39378d5d6ceb697cb17c6b940564df
venkat <techvenkats@gmail.com>
2024-12-01 20:29:22 +0530

    Update and rename __middleware.ts to middleware.ts

--- src/__middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export function middleware(request: NextRequest) {
-    const { pathname } = request.nextUrl;
-    if (pathname === "/" || pathname === "/dashboard") {
-        return NextResponse.redirect(new URL("/dashboard/student", request.url));
-    }
-}
